b27a6e314df2845a43cfd29b3dc08efe34f46e3d
Cenote <himangshukalita131@gmail.com>
2024-05-24 16:21:26 +0530

    Import some necessary middleware

--- backend/app.js
+++ backend/app.js
@@ -1,10 +1,19 @@
 import express from "express"
 import dotenv from "dotenv"
+import ErrorHandler from "./utils/ErrorHandler.js"
+import cookieParser from "cookie-parser"
+import bodyParser from "body-parser"
+import fileUpload from "express-fileupload"
 
 dotenv.config({
   path: "backend/config/.env",
 })
 
 const app = express()
+
+app.use(express.json())
+app.use(cookieParser())
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(fileUpload({ useTempFiles: true }))
 
 export default app
